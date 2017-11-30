bd30a05b7ff79d19501ec7dc04c6cdf2885f16d2
nadira.nuri <nadira.nuri@gmail.com>
2017-11-30 22:42:11 +0600

    t 6 20171130

--- www/static/survey.js
+++ www/static/survey.js
@@ -3723,7 +3723,7 @@ function uploadPhoto(imageURI, imageName) {
 function win(r) {
 
 	file_upload_error = 0;
-	
+	alert ('test')
 //	step_flag=0; //1 fd , 2 qpds, 3 giftuniext
 	
 	if (localStorage.step_flag==1){ //for fixed display
