7f63965c7d88f06eff41754f60a6df2184bbb606
nadira.nuri <nadira.nuri@gmail.com>
2018-03-10 13:57:15 +0600

    t 5 20180310

--- www/static/survey.js
+++ www/static/survey.js
@@ -4915,7 +4915,7 @@ function submit_data1() {
 						 url:  apipath+'zero_test?cid='+localStorage.cid+'&cm_id='+localStorage.cm_id+'&cm_pass='+localStorage.cm_pass+'&synccode='+localStorage.synccode+'&outlet='+localStorage.selectedOutlet+'&channel='+localStorage.outletChannel+'&strGet='+localStorage.submit_data1,
 						 
 						 success: function(resultS1) {	
-						 			
+						 		alert (resultS1)	
 						 		
 								var resultS=resultS1.split('<rdrd>')[0]
 								var fSL=resultS1.split('<rdrd>')[1]
